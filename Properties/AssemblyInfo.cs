7865eca8e7842835d5a686d8636498109335781d
Declan ter Veer-Burke <declantvb@gmail.com>
2023-05-19 16:40:04 +1200

    release 0.1.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using Vintagestory.API.Common;
 
 [assembly: ModDependency("game", "1.18.0")]
 [assembly: ModDependency("survival", "1.18.0")]
-[assembly: ModInfo("Bigger Windmill", "biggerwindmill", Version = "0.1.0", Authors = new string[] { "KingPin" }, Description = "Adds a bigger variant of the windmill.")]
+[assembly: ModInfo("Bigger Windmill", "biggerwindmill", Version = "0.1.1", Authors = new string[] { "KingPin" }, Description = "Adds a bigger variant of the windmill.")]
